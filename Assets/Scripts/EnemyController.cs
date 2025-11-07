6f88d710d7ea0b8dd548f032c22db7426f67a6c7
min-just-do-it <rlaalslw@gmail.com>
2025-11-07 19:37:59 +0900

    UI 캐릭터 이동
    TODO: 왼쪽이동 막기 저주상태일때, 카메라, UI 버튼연결

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,16 @@
+// ============================================
+// [EnemyController.cs]
+// Role : 적의 상태(HP, Ground, Move 등)
+// Input : EnemyController (상태 갱신)
+// Output : UIManager (HP 표시), GameManager (죽음 알림)
+// Event : OnHealthChanged, OnEnemyDead
+// ============================================
+
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+public class EnemyController : MonoBehaviour
+{
+    
+}
